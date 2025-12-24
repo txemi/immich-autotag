47d1086314a99b26fc2ce9cb4a85c5a2dba97a32
txemi <txemitron@gmail.com>
2025-12-24 15:29:05 +0100

    Add ImmichContext class for managing client and collections

---
+++ immich_autotag/core/immich_context.py
@@ -0,0 +1,17 @@
+import attrs
+from immich_client import Client
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .album_response_wrapper import AlbumCollectionWrapper
+    from .tag_collection_wrapper import TagCollectionWrapper
+
+@attrs.define(auto_attribs=True, slots=True, frozen=True)
+class ImmichContext:
+    client: Client = attrs.field(validator=attrs.validators.instance_of(Client))
+    albums_collection: "AlbumCollectionWrapper" = attrs.field(
+        validator=attrs.validators.instance_of(object)
+    )
+    tag_collection: "TagCollectionWrapper" = attrs.field(
+        validator=attrs.validators.instance_of(object)
+    )
