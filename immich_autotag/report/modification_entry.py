d7c4add9e915b51263cc89823559eddfba02f66c
txemi <txemitron@gmail.com>
2026-01-02 13:36:13 +0100

    Refactor ModificationEntry to improve type checking and simplify album field handling

--- immich_autotag/report/modification_entry.py
+++ immich_autotag/report/modification_entry.py
@@ -8,7 +8,11 @@ from typing import Any, Optional
 
 import attrs
 
-from immich_autotag.albums.album_response_wrapper import AlbumResponseWrapper
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from immich_autotag.albums.album_response_wrapper import AlbumResponseWrapper
+
+
 from immich_autotag.tags.modification_kind import ModificationKind
 from immich_autotag.report.serializable_modification_entry import SerializableModificationEntry
 from immich_autotag.users.user_response_wrapper import UserResponseWrapper
@@ -27,7 +31,7 @@ class ModificationEntry:
     kind: ModificationKind = attrs.field(validator=attrs.validators.instance_of(ModificationKind))
     asset_wrapper: Any = attrs.field(default=None)
     tag: Optional["TagResponseWrapper"] = attrs.field(default=None, validator=attrs.validators.optional(attrs.validators.instance_of(object)))
-    album: Optional[AlbumResponseWrapper] = attrs.field(default=None, validator=attrs.validators.optional(attrs.validators.instance_of(AlbumResponseWrapper)))
+    album: Optional["AlbumResponseWrapper"] = attrs.field(default=None)
     old_value: Any = attrs.field(default=None)
     new_value: Any = attrs.field(default=None)
     user: Optional[UserResponseWrapper] = attrs.field(default=None, validator=attrs.validators.optional(attrs.validators.instance_of(UserResponseWrapper)))
@@ -40,14 +44,16 @@ class ModificationEntry:
         Converts the rich entry to a serializable version (only simple types).
         The link is not stored, but can be derived from asset/album/user if needed.
         """
+        album_id = self.album.album.id if self.album is not None else None
+        album_name = self.album.album.album_name if self.album is not None else None
         return SerializableModificationEntry(
             datetime=self.datetime.isoformat(),
             kind=self.kind.name,
             asset_id=self.asset_wrapper.id_as_uuid if self.asset_wrapper is not None else None,
             asset_name=self.asset_wrapper.original_file_name if self.asset_wrapper is not None else None,
             tag_name=self.tag.tag_name if self.tag is not None else None,
-            album_id=self.album.album.id if self.album is not None else None,
-            album_name=self.album.album.album_name if self.album is not None else None,
+            album_id=album_id,
+            album_name=album_name,
             old_value=str(self.old_value) if self.old_value is not None else None,
             new_value=str(self.new_value) if self.new_value is not None else None,
             user_id=self.user.user_id if self.user is not None else None,
