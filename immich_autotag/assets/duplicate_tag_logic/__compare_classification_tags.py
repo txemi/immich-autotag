72fbb517ce165fe2fc98df37d5328ba3af3c83ec
txemi <txemitron@gmail.com>
2026-01-03 21:55:34 +0100

    Add import for _ClassificationTagComparisonResult in compare_classification_tags

--- immich_autotag/assets/duplicate_tag_logic/__compare_classification_tags.py
+++ immich_autotag/assets/duplicate_tag_logic/__compare_classification_tags.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Tuple, Optional, Set, Any
 if TYPE_CHECKING:
     from immich_autotag.assets.asset_response_wrapper import AssetResponseWrapper
 from ._classification_tag_comparison_result_obj import _ClassificationTagComparisonResultObj
+from ._classification_tag_comparison_result import _ClassificationTagComparisonResult
 @typechecked
 def compare_classification_tags(asset1: 'AssetResponseWrapper', asset2: 'AssetResponseWrapper') -> _ClassificationTagComparisonResultObj:
     """
