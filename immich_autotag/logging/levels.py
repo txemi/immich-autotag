45daf41a73fed191a1b907ee053bac95c6c86625
txemi <txemitron@gmail.com>
2026-01-08 19:09:10 +0100

    Add ERROR log level to LogLevel enum for enhanced logging granularity

--- immich_autotag/logging/levels.py
+++ immich_autotag/logging/levels.py
@@ -1,7 +1,9 @@
 from enum import Enum, auto
 
 
+
 class LogLevel(Enum):
+    ERROR = 40  # Error events that should be logged at a high level
     IMPORTANT = 30  # Important events, warnings, conflicts
     PROGRESS = 20  # General progress, batch info
     FOCUS = 15  # For focus mode: all relevant info for a specific asset (menos prioritario que PROGRESS)
