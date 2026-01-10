27447bcb6c121c97d1205f3780c221627b95ed58
txemi <txemitron@gmail.com>
2026-01-10 17:23:35 +0100

    Update version to 0.40.4 and git commit information in version.py

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,3 +1,4 @@
 # Este archivo se actualiza automáticamente en el proceso de build/distribución
-__version__ = "0.30"
-__git_commit__ = "c82598e"
+__version__ = "0.40.4"
+__git_commit__ = "33c33da"
+__git_describe__ = "v0.30-57-g33c33da-dirty"
