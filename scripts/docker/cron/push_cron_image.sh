db6156f69eaa5fa76ba7e9be297d1fc671d7d213
txemi <txemitron@gmail.com>
2026-01-10 18:18:08 +0100

    Enhance Docker scripts to support versioned image tagging and improve push output

--- scripts/docker/cron/push_cron_image.sh
+++ scripts/docker/cron/push_cron_image.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Push the cron-enabled Docker image to Docker Hub
+# Push all versioned cron Docker images to Docker Hub
 set -e
+REPO_ROOT="$(cd "$(dirname "$0")/../../../" && pwd)"
+VERSION=$(grep '^version' "$REPO_ROOT/pyproject.toml" | head -1 | cut -d'=' -f2 | tr -d ' "')
+HASH=$(git -C "$REPO_ROOT" rev-parse --short HEAD)
+
 docker push txemi/immich-autotag:cron
-echo "\nPushed image: txemi/immich-autotag:cron"
+docker push txemi/immich-autotag:cron-$VERSION
+docker push txemi/immich-autotag:cron-$VERSION-$HASH
+
+echo "\nPushed images: txemi/immich-autotag:cron, txemi/immich-autotag:cron-$VERSION, txemi/immich-autotag:cron-$VERSION-$HASH"
+echo "[INFO] View your images at: https://hub.docker.com/r/txemi/immich-autotag/tags"
