48dd0e423cb387b45b9abfc4765b165b55ae496f
txemi <txemitron@gmail.com>
2026-01-11 20:33:28 +0100

    Update version info for 0.66.0

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
-__version__ = "0.64.0"
-__git_commit__ = "319002b"
-__git_describe__ = "v0.64.0-0-g319002b-dirty"
+__version__ = "0.66.0"
+__git_commit__ = "9a4a47d"
+__git_describe__ = "v0.66.0-0-g9a4a47d-dirty"
