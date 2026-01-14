6584b45a6a018379b94120365bef64a091a08faa
txemi <txemitron@gmail.com>
2026-01-14 16:50:59 +0100

    Update version.py with tag v0.71.0 information

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
 __version__ = "0.71.0"
-__git_commit__ = "097ac98"
-__git_describe__ = "v0.70.7-57-g097ac98-dirty"
+__git_commit__ = "d09c116"
+__git_describe__ = "v0.71.0-0-gd09c116"
