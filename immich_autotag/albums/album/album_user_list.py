4e2f21a12589077af0d7ada049348347ebe08bb5
txemi <txemitron@gmail.com>
2026-01-21 21:35:10 +0100

    feat: add AlbumUserList class to manage a list of AlbumUserWrapper instances with type safety

---
+++ immich_autotag/albums/album/album_user_list.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Iterator
+
+import attrs
+from typeguard import typechecked
+
+if TYPE_CHECKING:
+    from .album_user_wrapper import AlbumUserWrapper
+
+
+@attrs.define(auto_attribs=True, slots=True)
+class AlbumUserList:
+    """
+    Encapsulates a list of AlbumUserWrapper objects, enforcing type safety and providing helper methods.
+    """
+
+    _users: list["AlbumUserWrapper"] = attrs.field(factory=list)
+
+    @typechecked
+    def __iter__(self) -> Iterator["AlbumUserWrapper"]:
+        return iter(self._users)
+
+    @typechecked
+    def __len__(self) -> int:
+        return len(self._users)
+
+    @typechecked
+    def __getitem__(self, idx: int) -> "AlbumUserWrapper":
+        return self._users[idx]
+
+    @typechecked
+    def append(self, user: "AlbumUserWrapper") -> None:
+        if not hasattr(user, "id"):
+            raise TypeError("Only AlbumUserWrapper instances can be added.")
+        self._users.append(user)
+
+    @typechecked
+    def to_id_list(self) -> list[str]:
+        return [u.id for u in self._users]
+
+    @typechecked
+    def to_uuid_list(self) -> list["UUID"]:
+        return [u.get_uuid() for u in self._users]
+
+    @typechecked
+    def emails(self) -> list[str]:
+        return [u.email for u in self._users]
+
+    @typechecked
+    def names(self) -> list[str]:
+        return [u.name for u in self._users]
