8eb073fd7241af9942b6a288a80cccff0f37d26f
txemi <txemitron@gmail.com>
2026-01-21 20:49:08 +0100

    feat: reorganize import statements in album_api_utils.py for improved clarity

--- immich_autotag/albums/album/album_api_utils.py
+++ immich_autotag/albums/album/album_api_utils.py
@@ -1,11 +1,15 @@
 """
 Utility functions for Immich album API calls (singular album).
 """
+
 from uuid import UUID
+
 from immich_client.api.albums import get_album_info
 from immich_client.models.album_response_dto import AlbumResponseDto
-from immich_autotag.types import ImmichClient
 from typeguard import typechecked
+
+from immich_autotag.types import ImmichClient
+
 
 @typechecked
 def get_album_info_by_id(album_id: UUID, client: ImmichClient) -> AlbumResponseDto:
