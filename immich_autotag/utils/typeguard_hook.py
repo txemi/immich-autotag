7289329b55f48e1e1965f3455eb1c7c9a517f533
txemi <txemitron@gmail.com>
2026-01-23 12:26:53 +0100

    feat: simplify typeguard import hook installation documentation

--- immich_autotag/utils/typeguard_hook.py
+++ immich_autotag/utils/typeguard_hook.py
@@ -13,17 +13,4 @@ def install_typeguard_import_hook(package_name: str = "immich_autotag"):
         import typeguard
         typeguard.install_import_hook(package_name)
     except ImportError:
-        pass"""
-Utilidad para instalar el import hook de typeguard de forma centralizada.
-"""
-
-def install_typeguard_import_hook(package_name: str = "immich_autotag"):
-    """
-    Instala el import hook de typeguard para el paquete indicado.
-    Si typeguard no está instalado, no hace nada.
-    """
-    try:
-        import typeguard
-        typeguard.install_import_hook(package_name)
-    except ImportError:
         pass
