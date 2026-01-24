4d57f59a410e7c2ea3bdbcf03b2743eaa25c94f5
txemi <txemitron@gmail.com>
2026-01-24 21:01:38 +0100

    refactor: add proxy function for retrieving the current user

---
+++ immich_autotag/api/immich_proxy/users.py
@@ -0,0 +1,7 @@
+from typing import Any
+from immich_client.api.users import get_my_user
+from immich_client.client import AuthenticatedClient
+from immich_client.models.user_admin_response_dto import UserAdminResponseDto
+
+def proxy_get_my_user(*, client: AuthenticatedClient) -> UserAdminResponseDto:
+    return get_my_user.sync(client=client)
