38634aed0446094843305214b63ab0506914ca31
txemi <txemitron@gmail.com>
2026-01-24 21:03:47 +0100

    refactor: add proxy functions for user search and removing users from albums

---
+++ immich_autotag/api/immich_proxy/permissions.py
@@ -0,0 +1,11 @@
+from typing import Any
+from immich_client.api.users import search_users
+
+from immich_client.api.albums import remove_user_from_album
+from immich_client.client import AuthenticatedClient
+def proxy_search_users(*, client: AuthenticatedClient) -> list[UserResponseDto]:
+    return search_users.sync(client=client, **kwargs)
+
+
+def proxy_remove_user_from_album(*, client: AuthenticatedClient) -> Any:
+    return remove_user_from_album.sync(client=client, **kwargs)
