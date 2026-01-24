306dec8c16be8e70cc79dff1dc9ee4c2a88d583e
txemi <txemitron@gmail.com>
2026-01-24 20:37:53 +0100

    refactor: reorder import statements for clarity in tags.py

--- immich_autotag/api/immich_proxy/tags.py
+++ immich_autotag/api/immich_proxy/tags.py
@@ -1,10 +1,11 @@
 from typing import List
 from uuid import UUID
 
-from immich_autotag.types import ImmichClient
 from immich_client.api.tags import tag_assets, untag_assets
 from immich_client.models.bulk_id_response_dto import BulkIdResponseDto
 from immich_client.models.bulk_ids_dto import BulkIdsDto
+
+from immich_autotag.types import ImmichClient
 
 
 def proxy_tag_assets(
