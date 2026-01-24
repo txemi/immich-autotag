da02e045d36c793e1a2c6379326c2362a94af48b
txemi <txemitron@gmail.com>
2026-01-24 21:05:28 +0100

    refactor: replace get_asset_duplicates with proxy_get_asset_duplicates for improved clarity

--- immich_autotag/duplicates/duplicates_loader.py
+++ immich_autotag/duplicates/duplicates_loader.py
@@ -2,8 +2,8 @@ from typing import Optional
 
 import attrs
 from immich_client import Client
-from immich_client.api.duplicates import get_asset_duplicates
 
+from immich_autotag.api.immich_proxy.duplicates import proxy_get_asset_duplicates
 from immich_autotag.duplicates.duplicate_collection_wrapper import (
     DuplicateCollectionWrapper,
 )
@@ -16,7 +16,7 @@ class DuplicatesLoader:
 
     def load(self):
         # Use the high-level client function to get duplicates
-        duplicates_dto_list = get_asset_duplicates.sync(client=self.client)
+        duplicates_dto_list = proxy_get_asset_duplicates(client=self.client)
         self.duplicates = DuplicateCollectionWrapper.from_api_response(
             duplicates_dto_list
         )
