7d28f2560e7460dd8b75ff02472093bd1be2086a
txemi <txemitron@gmail.com>
2026-01-24 20:25:06 +0100

    refactor: update client type to AuthenticatedClient in asset proxy functions

--- immich_autotag/api/immich_proxy/assets.py
+++ immich_autotag/api/immich_proxy/assets.py
@@ -4,19 +4,19 @@ Este módulo es el ÚNICO punto autorizado para interactuar con immich_client.ap
 """
 
 from uuid import UUID
-from immich_client import Client
+from immich_client import AuthenticatedClient
 from immich_client.api.assets import get_asset_info as _get_asset_info
 from immich_client.api.assets import update_asset as _update_asset
 from immich_client.models.asset_response_dto import AssetResponseDto
 from immich_client.models.update_asset_dto import UpdateAssetDto
 
-def get_asset_info(asset_id: UUID, client: Client) -> AssetResponseDto | None:
+def get_asset_info(asset_id: UUID, client: AuthenticatedClient) -> AssetResponseDto | None:
     """
     Wrapper centralizado para get_asset_info.sync.
     """
     return _get_asset_info.sync(id=asset_id, client=client)
 
-def update_asset(asset_id: UUID, client: Client, body: UpdateAssetDto) -> AssetResponseDto:
+def update_asset(asset_id: UUID, client: AuthenticatedClient, body: UpdateAssetDto) -> AssetResponseDto:
     """
     Wrapper centralizado para update_asset.sync.
     """
