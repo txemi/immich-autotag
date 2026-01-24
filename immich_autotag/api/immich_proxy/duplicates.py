f6e8c397820a8d2a64cdb527f49090a317a409d8
txemi <txemitron@gmail.com>
2026-01-24 21:00:45 +0100

    refactor: implement proxy function for retrieving asset duplicates

---
+++ immich_autotag/api/immich_proxy/duplicates.py
@@ -0,0 +1,6 @@
+from typing import Any
+from immich_client.api.duplicates import get_asset_duplicates
+from immich_client.client import AuthenticatedClient
+
+def proxy_get_asset_duplicates(*, client: AuthenticatedClient) -> list[DuplicateResponseDto] :
+    return get_asset_duplicates.sync(client=client)
