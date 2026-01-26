a3e867c438b5322dd1a43d3db5fad17aea9e9bce
txemi <txemitron@gmail.com>
2026-01-26 21:23:34 +0100

    refactor: correct import path for get_run_output_dir in setup_tee_logging function

--- immich_autotag/utils/tee_logging.py
+++ immich_autotag/utils/tee_logging.py
@@ -75,7 +75,7 @@ def setup_tee_logging(basename: str = "immich_autotag_full_output.log") -> None:
     (get_run_output_dir()), and the base name can be customized.
     """
     from immich_autotag.logging.utils import LogLevel, log
-    from immich_autotag.utils.run_output_dir import get_run_output_dir
+    from immich_autotag.run_output.run_output_dir import get_run_output_dir
 
     log_dir = get_run_output_dir()
     log_path = log_dir / basename
