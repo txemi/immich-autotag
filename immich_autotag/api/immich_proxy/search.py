261d13a08f53e818c79aa382354abd9349b17abb
txemi <txemitron@gmail.com>
2026-01-28 10:33:19 +0100

    refactor: update proxy_search_assets to use AuthenticatedClient for improved type safety

--- immich_autotag/api/immich_proxy/search.py
+++ immich_autotag/api/immich_proxy/search.py
@@ -2,9 +2,10 @@ from typing import Any
 
 from immich_client import Client
 from immich_client.api.search import search_assets
+from immich_client.client import AuthenticatedClient
 from immich_client.models.search_response_dto import SearchResponseDto
 from immich_client.types import Response
 
 
-def proxy_search_assets(*, client: Client, body: Any) -> Response[SearchResponseDto]:
+def proxy_search_assets(*, client: AuthenticatedClient, body: Any) -> Response[SearchResponseDto]:
     return search_assets.sync_detailed(client=client, body=body)
