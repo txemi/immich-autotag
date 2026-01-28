d42ce5de5838274c0cfe900a072259600573c59e
txemi <txemitron@gmail.com>
2026-01-28 22:43:58 +0100

    Refactor add_members_to_album: update to accept multiple users and adjust permission sync logic

--- immich_autotag/permissions/album_permission_executor.py
+++ immich_autotag/permissions/album_permission_executor.py
@@ -132,7 +132,7 @@ def _get_current_members(
 def add_members_to_album(
     *,
     album: AlbumResponseWrapper,
-    user: UserResponseWrapper,
+    users: List[UserResponseWrapper]    ,
     access_level: AlbumUserRole,
     context: ImmichContext,
 ) -> None:
@@ -142,7 +142,7 @@ def add_members_to_album(
 
     album_id = album.get_album_uuid()
     album_name = album.get_album_name()
-    user_id = user.get_id_as_str()
+    users_id =   [user.get_id_as_str() for user in users]
     user_ids = [user_id]
 
     log(
