ba8ab7bf5eabeceb8fc53c17b78d12515b5579d6
txemi <txemitron@gmail.com>
2026-01-28 11:35:09 +0100

    refactor: update album info caching to use ApiCacheKey for consistency

--- immich_autotag/api/immich_proxy/search.py
+++ immich_autotag/api/immich_proxy/search.py
@@ -1,6 +1,5 @@
-from typing import Any
 
-from immich_client.api.search import search_assets
+from immich_client.api.search.search_assets import sync_detailed as search_assets
 from immich_client.client import AuthenticatedClient
 from immich_client.models.metadata_search_dto import MetadataSearchDto
 from immich_client.models.search_response_dto import SearchResponseDto
@@ -10,4 +9,4 @@ from immich_client.types import Response
 def proxy_search_assets(
     *, client: AuthenticatedClient, body: MetadataSearchDto
 ) -> Response[SearchResponseDto]:
-    return search_assets.sync_detailed(client=client, body=body)
+    return search_assets(client=client, body=body)
