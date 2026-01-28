a7a7379173859a0db00487ba8e0da219b35f1b58
txemi <txemitron@gmail.com>
2026-01-28 21:08:31 +0100

    Refactor proxy_remove_user_from_album for improved readability and structure

--- immich_autotag/api/immich_proxy/permissions.py
+++ immich_autotag/api/immich_proxy/permissions.py
@@ -1,15 +1,17 @@
-
-from immich_client.types import Response
-
 from immich_client.api.albums import remove_user_from_album
 from immich_client.api.users import search_users
 from immich_client.client import AuthenticatedClient
 from immich_client.models.user_response_dto import UserResponseDto
+from immich_client.types import Response
 
 
 def proxy_search_users(*, client: AuthenticatedClient) -> list[UserResponseDto]:
     return search_users.sync(client=client)
 
 
-def proxy_remove_user_from_album(*, client: AuthenticatedClient, album_id: str, user_id: str) -> Response[Any]:
-    return remove_user_from_album.sync_detailed(client=client, id=album_id, user_id=user_id)
+def proxy_remove_user_from_album(
+    *, client: AuthenticatedClient, album_id: str, user_id: str
+) -> Response[Any]:
+    return remove_user_from_album.sync_detailed(
+        client=client, id=album_id, user_id=user_id
+    )
