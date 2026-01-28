18c065af8acf42c63c3c9f272d63912eeb7b78f1
txemi <txemitron@gmail.com>
2026-01-28 22:32:49 +0100

    Fix parameter name in ApiCacheManager.create method for clarity

--- immich_autotag/utils/api_disk_cache.py
+++ immich_autotag/utils/api_disk_cache.py
@@ -51,7 +51,7 @@ class ApiCacheManager:
         Use this instead of direct instantiation.
         """
         return ApiCacheManager(
-            _cache_type=cache_type,
+            cache_type=cache_type,
             use_cache=use_cache,
         )
 
