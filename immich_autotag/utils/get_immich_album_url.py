bf0f431b6a134f6b9203b8ac65dff1d7328023f9
txemi <txemitron@gmail.com>
2026-01-29 12:18:07 +0100

    Refactor import order in get_immich_album_url.py for improved readability

--- immich_autotag/utils/get_immich_album_url.py
+++ immich_autotag/utils/get_immich_album_url.py
@@ -1,16 +1,15 @@
 import uuid
 from urllib.parse import ParseResult, urlparse
 
-from typeguard import typechecked
 
+from typeguard import typechecked
 from immich_autotag.config.host_config import (
     IMMICH_PHOTO_PATH_TEMPLATE,
     get_immich_web_base_url,
 )
-
+from immich_autotag.assets.asset_uuid import AssetUUID
 
 @typechecked
-from immich_autotag.assets.asset_uuid import AssetUUID
 
 def get_immich_photo_url(asset_id: AssetUUID) -> ParseResult:
     """
