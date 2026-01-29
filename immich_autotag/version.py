58596d865a8411a908db3ddcb40de074371aab4c
txemi <txemitron@gmail.com>
2026-01-29 10:29:35 +0100

    Update version.py with tag v0.74.2 information

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
 __version__ = "0.74.2"
-__git_commit__ = "4a65c3c"
-__git_describe__ = "v0.74.1-qualitygate-45-g4a65c3c-dirty"
+__git_commit__ = "c93d6e4"
+__git_describe__ = "v0.74.2-0-gc93d6e4-dirty"
