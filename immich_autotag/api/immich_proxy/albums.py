0791d826d8e2351abf6b20a110c4b7ad757eed49
txemi <txemitron@gmail.com>
2026-01-29 13:01:32 +0100

    Refactor proxy_remove_asset_from_album to use AssetUUID for asset_ids

--- immich_autotag/api/immich_proxy/albums.py
+++ immich_autotag/api/immich_proxy/albums.py
@@ -77,7 +77,7 @@ def proxy_get_album_info(
 
 
 def proxy_remove_asset_from_album(
-    *, album_id: UUID, client: AuthenticatedClient, asset_ids: List[UUID]
+    *, album_id: UUID, client: AuthenticatedClient, asset_ids: List[AssetUUID]
 ) -> list[BulkIdResponseDto]:
     result = remove_asset_from_album.sync(
         id=album_id, client=client, body=BulkIdsDto(ids=asset_ids)
