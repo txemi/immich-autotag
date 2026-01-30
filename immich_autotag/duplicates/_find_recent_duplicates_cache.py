af9d03e1f219b9dd17ae932060461ebe56192a5e
txemi <txemitron@gmail.com>
2026-01-30 20:00:19 +0100

    refactor: update find_recent_duplicates_cache to use current instance of RunOutputManager for improved consistency

--- immich_autotag/duplicates/_find_recent_duplicates_cache.py
+++ immich_autotag/duplicates/_find_recent_duplicates_cache.py
@@ -21,7 +21,7 @@ def find_recent_duplicates_cache( max_age_hours: int) -> Optional[Path]:
 
     checked_dirs: list[DuplicatesCacheFile] = []
     candidate_caches: list[DuplicatesCacheFile] = []
-    for run_exec in RunOutputManager.find_recent_run_dirs(max_age_hours=max_age_hours):
+    for run_exec in RunOutputManager.current().find_recent_run_dirs(max_age_hours=max_age_hours):
         cache = DuplicatesCacheFile(run_execution=run_exec)
         checked_dirs.append(cache)
         if cache.exists():
