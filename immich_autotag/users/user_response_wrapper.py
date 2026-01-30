552216fe4521c83b75ac8a26033397e268d9e31e
txemi <txemitron@gmail.com>
2026-01-30 20:09:18 +0100

    refactor: streamline UserResponseWrapper by improving validation and method access

--- immich_autotag/users/user_response_wrapper.py
+++ immich_autotag/users/user_response_wrapper.py
@@ -14,46 +14,44 @@ if TYPE_CHECKING:
     from immich_autotag.context.immich_context import ImmichContext
 
 
-@attrs.define(auto_attribs=True, slots=True, frozen=True)
+@attrs.define(slots=True, frozen=True)
 class UserResponseWrapper:
+    @classmethod
+    def from_user(cls, user: Union["UserResponseDto", "UserAdminResponseDto"]) -> "UserResponseWrapper":
+        return UserResponseWrapper(user=user)
 
     @staticmethod
     def _validate_user(
         instance: "UserResponseWrapper",
-        attribute: attrs.Attribute["UserResponseDto"],
+        attribute: Any,
         value: object,
     ) -> None:
         if value is None:
             raise ValueError("user cannot be None")
-        try:
-            from immich_client.models.user_admin_response_dto import (
-                UserAdminResponseDto,
-            )
-            from immich_client.models.user_response_dto import UserResponseDto
-        except ImportError:
-            raise TypeError(
-                "UserResponseDto/UserAdminResponseDto type cannot be imported for validation"
-            )
+        from immich_client.models.user_admin_response_dto import (
+            UserAdminResponseDto,
+        )
+        from immich_client.models.user_response_dto import UserResponseDto
         if not isinstance(value, (UserResponseDto, UserAdminResponseDto)):
             raise TypeError(
                 f"user must be a UserResponseDto or UserAdminResponseDto, got "
                 f"{type(value)}"
             )
 
-    user: Union["UserResponseDto", "UserAdminResponseDto"] = attrs.field(
-        validator=_validate_user
+    _user: Union["UserResponseDto", "UserAdminResponseDto"] = attrs.field(
+        validator=_validate_user,
+        init=False
     )
     _cached_user_wrapper = None  # Class variable to cache the user
 
-    @property
     @typechecked
-    def name(self) -> str:
-        return self.user.name
+    def get_name(self) -> str:
+        return self._user.name
 
-    @property
     @typechecked
-    def email(self) -> str:
-        return self.user.email
+    def get_email(self):
+        from immich_autotag.types.email_wrapper import EmailWrapper
+        return EmailWrapper(self._user.email)
 
     @classmethod
     @typechecked
@@ -74,8 +72,8 @@ class UserResponseWrapper:
 
     @typechecked
     def get_uuid(self) -> UserUUID:
-        return UserUUID.from_string(self.user.id)
+        return UserUUID.from_string(self._user.id)
 
     @typechecked
     def __str__(self) -> str:
-        return self.name or self.id or "<unknown user>"
+        return self.get_name() or getattr(self._user, 'id', None) or "<unknown user>"
