6d2317e919d9a174114f9bbf30f7dc46425a6ea4
txemi <txemitron@gmail.com>
2026-01-30 18:03:40 +0100

    Bump version to 0.74.3-qualitygate

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
-__version__ = "0.74.2"
-__git_commit__ = "c93d6e4"
-__git_describe__ = "v0.74.2-0-gc93d6e4-dirty"
+__version__ = "0.74.3-qualitygate"
+__git_commit__ = "f220f5a9"
+__git_describe__ = "v0.74.2-225-gf220f5a9"
