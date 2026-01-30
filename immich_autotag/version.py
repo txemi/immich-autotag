5039286ec26d2428596609ca7c7035fe27a0f277
txemi <txemitron@gmail.com>
2026-01-30 18:03:40 +0100

    Update version.py with tag v0.74.3-qualitygate information

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
 __version__ = "0.74.3-qualitygate"
-__git_commit__ = "f220f5a9"
-__git_describe__ = "v0.74.2-225-gf220f5a9"
+__git_commit__ = "6d2317e9"
+__git_describe__ = "v0.74.3-qualitygate-0-g6d2317e9"
