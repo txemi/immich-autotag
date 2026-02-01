246122c0f44086d43c3b22541e7ba8426de86a37
txemi <txemitron@gmail.com>
2026-02-01 21:08:05 +0100

    refactor: Simplify function definition for proxy_get_my_user by removing unnecessary line breaks

--- immich_autotag/api/immich_proxy/users.py
+++ immich_autotag/api/immich_proxy/users.py
@@ -5,7 +5,5 @@ from immich_client.client import AuthenticatedClient
 from immich_client.models.user_admin_response_dto import UserAdminResponseDto
 
 
-def proxy_get_my_user(
-    *, client: AuthenticatedClient
-) -> Optional[UserAdminResponseDto]:
+def proxy_get_my_user(*, client: AuthenticatedClient) -> Optional[UserAdminResponseDto]:
     return get_my_user.sync(client=client)
