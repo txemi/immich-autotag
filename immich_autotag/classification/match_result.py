84e424ad4c49efd78ba551d17a1a250eeeb56f36
txemi <txemitron@gmail.com>
2026-02-02 20:51:34 +0100

    feat: Enhance MatchResult class with additional attributes and typechecked methods for improved functionality

--- immich_autotag/classification/match_result.py
+++ immich_autotag/classification/match_result.py
@@ -6,6 +6,7 @@ from typeguard import typechecked
 from immich_autotag.classification.validators import validate_classification_rule
 
 if TYPE_CHECKING:
+    from immich_autotag.assets.asset_response_wrapper import AssetResponseWrapper
     from immich_autotag.classification.classification_rule_wrapper import (
         ClassificationRuleWrapper,
     )
@@ -14,19 +15,23 @@ if TYPE_CHECKING:
 # Represents the result of a match: reference to the rule and the matched tags and albums
 @attr.s(auto_attribs=True, slots=True, kw_only=True, frozen=True)
 class MatchResult:
-    _rule: "ClassificationRuleWrapper" = attr.ib(validator=validate_classification_rule)
+    _rule: "ClassificationRuleWrapper" = attr.ib(validator=validate_classification_rule, alias="rule",init=True)
     _tags_matched: list[str] = attr.ib(
         factory=list,
         validator=attr.validators.instance_of(list),
+        alias="tags_matched",
     )
     _albums_matched: list[str] = attr.ib(
         factory=list,
         validator=attr.validators.instance_of(list),
+        alias="albums_matched",
     )
     _asset_links_matched: list[str] = attr.ib(
         factory=list,
         validator=attr.validators.instance_of(list),
+        alias="asset_links_matched",
     )
+    _asset: "AssetResponseWrapper" = attr.ib(alias="asset", init=True)
 
     def __attrs_post_init__(self):
         if not (self._tags_matched or self._albums_matched or self._asset_links_matched):
@@ -53,6 +58,11 @@ class MatchResult:
     def asset_links_matched(self) -> list[str]:
         """Returns the list of asset links matched by this rule."""
         return self._asset_links_matched
+
+    @typechecked
+    def asset(self) -> "AssetResponseWrapper":
+        """Returns the asset that produced this match result."""
+        return self._asset
 
     @typechecked
     def has_match(self) -> bool:
