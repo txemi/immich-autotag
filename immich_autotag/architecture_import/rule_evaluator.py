dd61b706848625f2f1052493b5eafc1cf44d4374
txemi <txemitron@gmail.com>
2026-02-04 11:20:48 +0100

    refactor: Simplify variable assignments and improve readability in evaluate_import_rules function

--- immich_autotag/architecture_import/rule_evaluator.py
+++ immich_autotag/architecture_import/rule_evaluator.py
@@ -19,12 +19,11 @@ def evaluate_import_rules(imported_module: ModulePath, caller_info: ModulePath) 
     Evaluate all architecture import rules for a given import.
     Takes the imported module (as ImportedModuleInfo) and the caller (as CallerInfo).
     """
-    my_caller_info: ImmichModulePath = ImmichModulePath.from_module_path(caller_info)
-    my_caller_2 = my_caller_info
-    if my_caller_2.is_outside_root_package():
+    caller: ImmichModulePath = ImmichModulePath.from_module_path(caller_info)
+    if caller.is_outside_root_package():
         return None
-    imported2: ImmichModulePath = ImmichModulePath.from_module_path(imported_module)
-    enforce_immich_api_import_rule(imported2, my_caller_2)
-    enforce_immich_proxy_import_rule(imported2, my_caller_2)
-    enforce_logging_proxy_import_rule(imported2, my_caller_2)
+    imported: ImmichModulePath = ImmichModulePath.from_module_path(imported_module)
+    enforce_immich_api_import_rule(imported, caller)
+    enforce_immich_proxy_import_rule(imported, caller)
+    enforce_logging_proxy_import_rule(imported, caller)
     return None
