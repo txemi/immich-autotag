557dc470d98a1757a6263d0dda30e00d5b8eb34d
txemi <txemitron@gmail.com>
2026-02-10 21:59:27 +0100

    refactor: Improve formatting of album renaming logic for consistency and readability

--- immich_autotag/albums/albums/duplicates_manager/rename_strategy/cleanup_rescue.py
+++ immich_autotag/albums/albums/duplicates_manager/rename_strategy/cleanup_rescue.py
@@ -43,10 +43,10 @@ def cleanup_album_names(album_collection: AlbumCollectionWrapper):
         current_name = album.get_album_name()
         if original_name and original_name != current_name:
             album_wrapper: AlbumResponseWrapper = album.rename_album(
-                    new_name=original_name,
-                    client=client,
-                    modification_report=tag_mod_report,
-                )
+                new_name=original_name,
+                client=client,
+                modification_report=tag_mod_report,
+            )
 
 
 # Entry point for rescue operation
--- immich_autotag/albums/albums/duplicates_manager/rename_strategy/rename_duplicate_album.py
+++ immich_autotag/albums/albums/duplicates_manager/rename_strategy/rename_duplicate_album.py
@@ -23,7 +23,7 @@ def rename_duplicate_album(
     """
     new_name = f"{album_wrapper.get_album_name()}{RENAMED_BY_AUTOTAG_DUPLICATE_USER_ALBUM_SUFFIX}"
     return album_wrapper.rename_album(
-            new_name=new_name,
-            client=client,
-            modification_report=tag_mod_report,
-        )
+        new_name=new_name,
+        client=client,
+        modification_report=tag_mod_report,
+    )
