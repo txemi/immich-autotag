f34602e4083fe8d7c57203c2a25254b648f102c0
txemi <txemitron@gmail.com>
2026-04-14 11:26:21 +0200

    Bump version to 0.80.5

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
-__version__ = "0.80.4"
-__git_commit__ = "4b9ae22d"
-__git_describe__ = "v0.80.4-0-g4b9ae22d"
+__version__ = "0.80.5"
+__git_commit__ = "c6ca793a"
+__git_describe__ = "jenkins-abort-30-c6ca793af710a4dee0eb44104c0faf8dc6a0f4b6-0-gc6ca793a-dirty"
