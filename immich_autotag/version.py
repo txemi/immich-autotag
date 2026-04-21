e337778b7b27857e526ba3a75af9b94566929766
txemi <txemitron@gmail.com>
2026-04-21 10:48:47 +0200

    Update version.py with tag v0.80.7 information

--- immich_autotag/version.py
+++ immich_autotag/version.py
@@ -1,4 +1,4 @@
 # This file is automatically updated in the build/distribution process
 __version__ = "0.80.7"
-__git_commit__ = "dadded17"
-__git_describe__ = "v0.80.6-41-gdadded17-dirty"
+__git_commit__ = "e15308cf"
+__git_describe__ = "v0.80.7-0-ge15308cf"
